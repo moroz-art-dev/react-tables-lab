8d35d6381455f54dbf6c260e3cb2454a9378dd0b
moroz-art-dev <moroz.art.dev@gmail.com>
2024-11-28 10:47:38 +0300

    Setup project and installed libraries. Added minimal Table component.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Table from './components/Table';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>AG Grid Table</h1>
+      <Table />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Table: React.FC = () => {
+  return <>Table</>;
+};
+
+export default Table;
